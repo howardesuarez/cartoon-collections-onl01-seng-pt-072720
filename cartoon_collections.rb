bef4c94467600bba6063a038c251c45e86c588dd
howard suarez <howardesuarez@gmail.com>
2020-08-07 02:40:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,3 +11,10 @@ def long_planeteer_calls(planeteer_calls)
   call.length > 4
   end
 end	
+
+def find_the_cheese(snacks)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find do |item|
+    cheese_types.include?(item)
+  end
+end	
