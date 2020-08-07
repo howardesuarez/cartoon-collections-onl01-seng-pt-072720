1ca76db2ef4de53ac8ec66baea11775afbeea69c
howard suarez <howardesuarez@gmail.com>
2020-08-07 02:04:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,3 @@
 def roll_call_dwarves(dwarves)
-  # Your code here	  dwarves.each_with_index {|dwarf, index| puts "#{index +1}. #{dwarf}"}
-end	end
+  dwarves.each_with_index {|dwarf, index| puts "#{index +1}. #{dwarf}"}
+end
