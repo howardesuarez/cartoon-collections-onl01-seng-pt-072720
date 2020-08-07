ceff65c8fcb921719d928c15e92e8bf769d0ef8a
howard suarez <howardesuarez@gmail.com>
2020-08-07 02:04:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,19 +1,3 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index {|dwarf, index| puts "#{index +1}. #{dwarf}"}
-end
-
-
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map { |planeteer| planeteer.capitalize + "!" }
-end
-
-
-describe "#long_planteer_calls" do
-
-
-def find_the_cheese(snacks)
- cheese_types = ["cheddar", "gouda", "camembert"]	  
-  snacks.find do |item|
-    cheese_types.include?(item)
-  end
-end
+  # Your code here	  dwarves.each_with_index {|dwarf, index| puts "#{index +1}. #{dwarf}"}
+end	end
