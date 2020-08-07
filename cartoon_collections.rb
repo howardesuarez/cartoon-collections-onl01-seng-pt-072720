23480fce47379f31a1d12cfdf1d9dc517bb32f44
howard suarez <howardesuarez@gmail.com>
2020-08-07 02:05:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,3 +2,7 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index {|dwarf, index| puts "#{index +1}. #{dwarf}"}
 end
 
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |planeteer| planeteer.capitalize + "!" }
+end	
+
